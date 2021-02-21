385e0967969e10290403c60fad8e96ef4b315860
KariukiAbel <abelmuriithi@gmail.com>
2021-02-21 12:06:34 +0300

    Still got the same error

--- readinFile.js
+++ readinFile.js
@@ -1,19 +1,28 @@
 var fs= require('fs'); //adds additonal functionality
 
-var file;
-var buf = new Buffer(100000);
+// var file;
+// var buf = new Buffer(100000);
 fs.open(
   'info.txt', 'r',
-  function handle() {
-      file = handle;
+  function(err, handle) {
+    var buf = new Buffer(100000);
+      // file = handle;
+      fs.read(
+        handle, buf, 0, 100000, null,
+        function (err, length) {
+          console.log(buf.toString('utf8', 0, length));
+          fs.close(handle, function () {
+          });
+        }
+      );
   }
 );
 
 
-fs.read(
-  file, buf, 0, 100000, null,
-  function () {
-    console.log(buf.toString());
-    file.close(file, function(){})
-  }
-);
+// fs.read(
+//   file, buf, 0, 100000, null,
+//   function () {
+//     console.log(buf.toString());
+//     file.close(file, function(){})
+//   }
+// );
