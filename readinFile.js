b9065ec877c36528ae76e67fb0dbd2d0094d9af6
KariukiAbel <abelmuriithi@gmail.com>
2021-03-16 02:46:36 +0300

    Added the handle_list_albums function to load_albums.js to handle all load_album_list requests

---
+++ readinFile.js
@@ -0,0 +1,31 @@
+var fs= require('fs'); //adds additonal functionality
+
+// var file;
+// var buf = new Buffer(100000);
+fs.open(
+  'info.txt', 'r',
+  function(err, handle) {
+    if(err){
+      console.log("ERROR: " + err.code + " ( " + err.message +" )")
+    }
+    var buf = new Buffer(100000);
+      // file = handle;
+      fs.read(
+        handle, buf, 0, 100000, null,
+        function (err, length) {
+          console.log(buf.toString('utf8', 0, length));
+          fs.close(handle, function () {
+          });
+        }
+      );
+  }
+);
+
+
+// fs.read(
+//   file, buf, 0, 100000, null,
+//   function () {
+//     console.log(buf.toString());
+//     file.close(file, function(){})
+//   }
+// );
